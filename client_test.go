a32ff5c170bbb6c8d590e675e07fcec6e2bb2bbe
Jack Lindamood <jack@signalfx.com>
2016-04-12 11:41:10 -0700

    Add some tests

--- client_test.go
+++ client_test.go
@@ -3,7 +3,31 @@ package smitego
 import (
 	"fmt"
 	"golang.org/x/net/context"
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+	"net/http"
+	"strings"
+	"io/ioutil"
 )
+
+type errorRT struct {
+}
+
+func (t *errorRT) RoundTrip(req *http.Request) (*http.Response, error) {
+	return nil, t
+}
+
+func (t *errorRT) Error() string {
+	return "An error in transport"
+}
+
+type staticResp struct {
+	Resp *http.Response
+}
+
+func (t *staticResp) RoundTrip(req *http.Request) (*http.Response, error) {
+	return t.Resp, nil
+}
 
 // Example of creating a session and making a function call.
 func ExampleSession() {
@@ -26,4 +50,36 @@ func ExampleSession() {
 	session, _ := client.CreateSession(ctx)
 	gods, _ := session.GetGods(ctx, English)
 	fmt.Printf("Got %d gods\n", len(gods))
+}
+
+func TestClient(t *testing.T) {
+	Convey("With a client", t, func() {
+		c := Client {
+		}
+		ctx := context.Background()
+		Convey("that errors http connections", func() {
+			rt := &errorRT{}
+			c.HTTPClient.Transport = rt
+			Convey("Connections should error", func() {
+				err := c.Ping(ctx)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldContainSubstring, rt.Error())
+			})
+		})
+		Convey("that 404s", func() {
+			sr := &staticResp{
+				Resp: &http.Response{
+					StatusCode: http.StatusNotFound,
+					Body: ioutil.NopCloser(strings.NewReader("")),
+				},
+			}
+			c.HTTPClient.Transport = sr
+			Convey("requests should error", func() {
+				err := c.Ping(ctx)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldContainSubstring, fmt.Sprintf("%d", http.StatusNotFound))
+			})
+		})
+
+	})
 }
